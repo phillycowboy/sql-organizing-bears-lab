2158a28217a200a9b6e8e74602d993cab76352b9
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-12-07 16:30:30 -0500

    commit tree a3a9aa8145a126dcbb2b2ec7353abd82665461d3

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1,"Mr. Chocolate", 4, "M", "Brown", "happy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2,"Rowdy", 8, "M", "Black", "excited", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3,"Tabitha", 6, "F", "Brown", "sweet", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4,"Sergeant Brown", 17, "M", "red", "meh", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5,"Melissa", 9, "F", "Dark Brown", "lovable", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6,"Grinch", 12, "M", "Black", "Grinchy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7,"Wendy", 4, "F", "Black", "happy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 20, "M", "Brown", "mean", 1);
